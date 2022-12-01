07d03fb9499900369efb1ca6e6d110d36b8321bd
Vale González <valeysa30@hotmail.com>
2022-12-01 17:57:18 -0600

    add ui elements to reuse throughout page

---
+++ src/components/ui/Article/Article.tsx
@@ -0,0 +1,41 @@
+import { MutableRefObject } from "react";
+
+export default function Article(props: any) {
+	return <article className="">{props.children}</article>;
+}
+
+export const ArticleBanner: React.FC<{ path: string }> = ({ path }) => {
+	return <img className="w-screen h-[320px] object-cover" src={path} alt="" />;
+};
+
+export const ArticleImage: React.FC<{
+	width?: number;
+	height?: number;
+	path: string;
+	float?: "left" | "right";
+}> = ({ width, height, path, float }) => {
+	return (
+		<img
+			width={width}
+			height={height}
+			className={`float-${float ? float + " mx-2" : "none m-auto"}`}
+			src={path}
+			alt=""
+		/>
+	);
+};
+
+export const ArticleTitle: React.FC<{
+	linkRef?: MutableRefObject<HTMLHeadingElement | null>;
+	title: string;
+}> = ({ linkRef, title }) => {
+	return (
+		<h1 ref={linkRef} className="text-4xl mt-3 font-semibold text-violet-700">
+			{title}
+		</h1>
+	);
+};
+
+export const ArticleContent: React.FC<{ children: any }> = ({ children }) => {
+	return <div className="">{children}</div>;
+};
